d93a4ea5c0d637f7a3c1f5c828103bffa21854bc
jnannie <jnannie@student.21-school.ru>
2020-12-27 13:44:52 +0300

    module07 ex00 is finished

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+//
+// Created by jnannie on 12/27/20.
+//
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template<typename T> void swap(T &a, T &b) {
+	T c = a;
+
+	a = b;
+	b = c;
+}
+
+template<typename T> T min(T &a, T &b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+template<typename T> T max(T &a, T &b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif //WHATEVER_HPP
